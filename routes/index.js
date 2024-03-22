d2d8babf7d0e8bbe35d455d6de14ff7952a83eda
Ben Barnett <ben@brushfiredesign.net>
2024-03-22 11:35:07 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ exports.index = function (req, res, next) {
 
 exports.admin = function (req, res, next) {
   console.log(req.body);
-  User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+  User.find({ username: req.query.username, password: req.body.password }, function (err, users) {
     if (users.length > 0) {
       return res.render('admin', {
         title: 'Admin Access Granted',
